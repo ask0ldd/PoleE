ac15c1256c09db83c59814cace922f7c41ca8287
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-04-12 04:24:48 +0200

    *edit* front / main component retrieving an access token from the third party api

--- front/src/app/app.component.ts
+++ front/src/app/app.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HomeComponent } from "./pages/home/home.component";
+import { JobsAPIService } from './services/mocks/jobsAPI.service';
+import { CookiesService } from './services/cookies.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -13,12 +16,19 @@ export class AppComponent implements OnInit, OnDestroy{
   // allows us to use HomeComponent within app.component.html
   @ViewChild(HomeComponent) homeComponent! : HomeComponent
 
+  subscription! : Subscription
+
+  constructor(private jobsAPI: JobsAPIService, private cookiesService : CookiesService){ }
+
   ngOnInit(): void {
     // retrieve the user id from the claims into the jwt
-    
+    this.subscription = this.jobsAPI.getAccessToken().subscribe(token => {
+      if(!token) return console.log("Can't retrieve the job API token.")
+      this.cookiesService.saveToken(token)
+    })
   }
 
   ngOnDestroy(): void {
-   
+    this.subscription.unsubscribe()
   }
 }
