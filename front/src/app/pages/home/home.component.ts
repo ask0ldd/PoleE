044ed6a7ef88fc0d58ab14c194a427cf7165d1fb
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-04-12 04:23:28 +0200

    *edit* front / home component : jobs list

--- front/src/app/pages/home/home.component.ts
+++ front/src/app/pages/home/home.component.ts
@@ -1,11 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { JobsAPIService } from '../../services/mocks/jobsAPI.service';
+import { Subscription } from 'rxjs';
+import IJobOffer from '../../interfaces/jobsAPI/IJobOffer';
+import { TruncTitlePipe } from '../../pipes/trunc-title.pipe';
+import { LowerCasePipe, TitleCasePipe } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [TruncTitlePipe, TitleCasePipe, LowerCasePipe],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
 
+  subscription! : Subscription
+  jobsOffers! : IJobOffer[]
+
+  constructor(private jobsAPIService : JobsAPIService){}
+  ngOnInit(): void {
+    this.subscription = this.jobsAPIService.getAll().subscribe(
+      jobs => this.jobsOffers = jobs
+    )
+  }
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
+  }
+  
 }
