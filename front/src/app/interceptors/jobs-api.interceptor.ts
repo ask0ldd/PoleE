c958f77918100be7886d76fc5a06c304c64ac7c5
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-04-14 22:45:11 +0200

    *edit* front / jobs api interceptor : token refresh

--- front/src/app/interceptors/jobs-api.interceptor.ts
+++ front/src/app/interceptors/jobs-api.interceptor.ts
@@ -1,36 +1,42 @@
 import { HttpEvent, HttpHandlerFn, HttpInterceptorFn, HttpRequest } from '@angular/common/http';
-import { Observable, take } from 'rxjs';
+import { catchError, filter, Observable, of, switchMap, take, tap } from 'rxjs';
 import { inject } from '@angular/core';
 import { ThirdPartyTokenService } from '../services/third-party-token.service';
 
+const TOKEN_REFRESH_THRESHOLD = 5000
+
 export const jobsAPIInterceptor: HttpInterceptorFn = (req: HttpRequest<unknown>, next : HttpHandlerFn) : Observable<HttpEvent<unknown>> => {
-  const thirdPartyTokenService = inject(ThirdPartyTokenService)
 
-  let token : string | null
+  const thirdPartyTokenService = inject(ThirdPartyTokenService);
 
-  /*function refreshToken(){
-    thirdPartyTokenService.fetchAccessToken().pipe(take(1)).subscribe(tk => {
-      token = tk
-      if(tk) thirdPartyTokenService.setToken(tk)
-    })
-  }*/
-  
-  try{
-    if (req.url.includes("/jobsapi/")) {
-      token = thirdPartyTokenService.getToken()
-
-      const clonedRequest = req.clone({
-        setHeaders: {
-          Authorization: `Bearer ${token}`,
-        },
-      })
-
-      return next(clonedRequest)
-    }
-
-    return next(req)
-  } catch(e) {
-    if(e instanceof Error) console.error(e.message)
-    return next(req)
+  // Skip interception for non-JobsAPI requests
+  if (!req.url.includes("/jobsapi/")) {
+    return next(req);
   }
+
+  const storageToken = thirdPartyTokenService.getToken()
+  const expirationDate = thirdPartyTokenService.getExpirationDate()
+  
+  // don't refresh the token if it expires in more than 5 secs
+  if(storageToken != null && expirationDate && (expirationDate - Date.now() > TOKEN_REFRESH_THRESHOLD)) {
+    const requestToForward = req.clone({
+      setHeaders: { Authorization: `Bearer ${storageToken}` }
+    })
+    return next(requestToForward)
+  }
+
+  return thirdPartyTokenService.fetchAccessToken().pipe(
+    filter(token => token != null),
+    take(1),
+    catchError(error => {
+      console.error('Failed to fetch access token:', error);
+      return of(null);
+    }),
+    switchMap(token => {
+      const requestToForward = token 
+      ? req.clone({ setHeaders: { Authorization: `Bearer ${token}` } })
+      : req
+      return next(requestToForward)
+    })
+  );
 }
