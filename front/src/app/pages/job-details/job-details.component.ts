7186fa12872f7a2f1f7ad4e5b54b3f913d8e4ba4
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-04-16 21:40:35 +0200

    *edit* front / fixing job details markdown rendering

--- front/src/app/pages/job-details/job-details.component.ts
+++ front/src/app/pages/job-details/job-details.component.ts
@@ -1,11 +1,12 @@
 import { Component, DestroyRef, inject, Input, OnInit } from '@angular/core';
 import IJobOffer from '../../interfaces/jobsAPI/IJobOffer';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { take } from 'rxjs';
+import { map, of, switchMap, take } from 'rxjs';
 import { LowerCasePipe } from '@angular/common';
 import { ToElapsedDaysPipe } from '../../pipes/to-elapsed-days.pipe';
 import { JobsAPIService } from '../../services/mocks/jobs-api.service';
 import { CapitalizeFirstLetterPipe } from '../../pipes/capitalize-first-letter.pipe';
+import { marked } from "marked"
 
 @Component({
   selector: 'app-job-details',
@@ -20,12 +21,15 @@ export class JobDetailsComponent implements OnInit {
 
   constructor(private jobsAPIService : JobsAPIService){ }
 
-  ngOnInit() {
+  async ngOnInit() {
+    let offer : IJobOffer
     this.jobsAPIService.getById(this.id).pipe(
       take(1),
       takeUntilDestroyed(this.destroyRef),
-    ).subscribe(
-      job => this.jobOffer = job
+    )
+    .pipe(switchMap(async (offer) => ({...offer, description : await marked(offer.description.replaceAll("***", "\n\n"))})))
+    .subscribe(
+      offer => this.jobOffer = offer
     )
   }
 }
