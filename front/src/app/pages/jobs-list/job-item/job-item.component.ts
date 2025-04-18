7e67f0e35e1a3f50f5bfd0a3a4dbff24ecf5b319
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-04-18 19:21:43 +0200

    *edit* front / job item styling

--- front/src/app/pages/jobs-list/job-item/job-item.component.ts
+++ front/src/app/pages/jobs-list/job-item/job-item.component.ts
@@ -5,10 +5,11 @@ import { RouterLink } from '@angular/router';
 import { CapitalizeFirstLetterPipe } from '../../../pipes/capitalize-first-letter.pipe';
 import { ToElapsedDaysPipe } from '../../../pipes/to-elapsed-days.pipe';
 import { TruncTitlePipe } from '../../../pipes/trunc-title.pipe';
+import { SalaryParsingPipe } from '../../../pipes/salary-parsing.pipe';
 
 @Component({
   selector: 'app-job-item',
-  imports: [TruncTitlePipe, LowerCasePipe, ToElapsedDaysPipe, RouterLink, CapitalizeFirstLetterPipe],
+  imports: [TruncTitlePipe, LowerCasePipe, ToElapsedDaysPipe, RouterLink, CapitalizeFirstLetterPipe, SalaryParsingPipe],
   templateUrl: './job-item.component.html',
   styleUrl: './job-item.component.css'
 })
