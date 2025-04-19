303d3171fda117d1c89cb1c175bac5be2a0f1c08
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-04-19 18:11:40 +0200

    *add* back / favorite module

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { FavoritesModule } from './jobs/favorites/favorites.module';
 
 @Module({
-  imports: [],
+  imports: [FavoritesModule],
   controllers: [AppController],
   providers: [AppService],
 })
