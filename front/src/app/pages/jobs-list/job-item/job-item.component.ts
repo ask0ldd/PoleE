5d5c3b50ff4dcd04f055b8112d78694abf0bb79c
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-04-19 18:03:03 +0200

    *edit* front / job item styling

--- front/src/app/pages/jobs-list/job-item/job-item.component.ts
+++ front/src/app/pages/jobs-list/job-item/job-item.component.ts
@@ -1,7 +1,6 @@
-import { Component, Input } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
 import IJobOffer from '../../../interfaces/jobsAPI/IJobOffer';
 import { LowerCasePipe } from '@angular/common';
-import { RouterLink } from '@angular/router';
 import { CapitalizeFirstLetterPipe } from '../../../pipes/capitalize-first-letter.pipe';
 import { ToElapsedDaysPipe } from '../../../pipes/to-elapsed-days.pipe';
 import { TruncTitlePipe } from '../../../pipes/trunc-title.pipe';
@@ -9,10 +8,15 @@ import { SalaryParsingPipe } from '../../../pipes/salary-parsing.pipe';
 
 @Component({
   selector: 'app-job-item',
-  imports: [TruncTitlePipe, LowerCasePipe, ToElapsedDaysPipe, RouterLink, CapitalizeFirstLetterPipe, SalaryParsingPipe],
+  imports: [TruncTitlePipe, LowerCasePipe, ToElapsedDaysPipe, CapitalizeFirstLetterPipe, SalaryParsingPipe],
   templateUrl: './job-item.component.html',
   styleUrl: './job-item.component.css'
 })
 export class JobItemComponent {
   @Input() jobOffer! : IJobOffer
+  @Output() emitJob = new EventEmitter<string>()
+
+  emitActiveJobId(){
+    this.emitJob.emit(this.jobOffer.id)
+  }
 }
