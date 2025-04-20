fdbbefc7ee1ef3123daa815d972a33c02a97d40d
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-04-20 04:19:36 +0200

    *add* back / sqlite + typeorm install

--- back/src/app.module.ts
+++ back/src/app.module.ts
@@ -2,9 +2,18 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { FavoritesModule } from './jobs/favorites/favorites.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [FavoritesModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'database.sqlite',
+      entities: [__dirname + '/**/*.entity{.ts,.js}'], // import all entities across the project
+      synchronize: true,
+    }),
+    FavoritesModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
