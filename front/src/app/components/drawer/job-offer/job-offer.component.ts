9f235e45a369ceaa1bfd45451180d334a44a99bb
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-04-24 03:52:37 +0200

    *edit* front / job offer : logic and directives integrating llm generated data

--- front/src/app/components/drawer/job-offer/job-offer.component.ts
+++ front/src/app/components/drawer/job-offer/job-offer.component.ts
@@ -1,8 +1,10 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, OnDestroy, OnInit, SimpleChanges } from '@angular/core';
 import IJobOffer from '../../../interfaces/jobsAPI/IJobOffer';
 import { ToElapsedDaysPipe } from "../../../pipes/to-elapsed-days.pipe";
 import { LowerCasePipe } from '@angular/common';
 import { CapitalizeFirstLetterPipe } from '../../../pipes/capitalize-first-letter.pipe';
+import { LlmService } from '../../../services/llm.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-job-offer',
@@ -10,6 +12,23 @@ import { CapitalizeFirstLetterPipe } from '../../../pipes/capitalize-first-lette
   templateUrl: './job-offer.component.html',
   styleUrl: './job-offer.component.css'
 })
-export class JobOfferComponent {
+export class JobOfferComponent implements OnInit, OnDestroy, OnChanges{
   @Input() jobOffer? : IJobOffer
+  summarizedOffer : string = ""
+  sub! : Subscription
+
+  constructor(private llmService : LlmService){ }
+
+  ngOnInit(){
+    this.sub = this.llmService.output$.subscribe(gen => this.summarizedOffer = gen == "" ? "" : JSON.stringify(gen))
+    if(this.jobOffer) this.llmService.generate(this.jobOffer.description)
+  }
+
+  ngOnChanges() {
+    if(this.jobOffer) this.llmService.generate(this.jobOffer.description)
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe()
+  }
 }
