e11e4c66b8bf01deb75b587c32251b3efa6120d6
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-04-24 03:49:36 +0200

    *edit* front / llm service : - configuring transformers js so it accepts local models - parsing llm generated datas + type checking

--- front/src/app/services/llm.service.ts
+++ front/src/app/services/llm.service.ts
@@ -1,20 +1,27 @@
 import { Injectable } from '@angular/core';
-import { pipeline, TextGenerationPipeline } from '@huggingface/transformers'
-import { Subject } from 'rxjs';
+import { pipeline, TextGenerationPipeline, env, Chat, TextGenerationOutput } from '@huggingface/transformers'
+import { map, Subject } from 'rxjs';
+
+// https://github.com/huggingface/transformers.js/blob/main/docs/source/pipelines.md
+
+// dev mode : local model
+env.allowRemoteModels = false
+env.allowLocalModels = true
+env.localModelPath = 'http://localhost:3000/static/model'
 
 @Injectable({
   providedIn: 'root'
 })
 export class LlmService {
 
-  output$ = new Subject()
+  output$ = new Subject<string>()
+  i : number = 0
 
   generator : TextGenerationPipeline | null = null
   messages = [
-    { role: "system", content: "You are a helpful assistant." },
-    { role: "user", content: "Tell me a funny joke." },
+    { role: "system", content: "You are a helpful assistant. Your goal is to summarize any text the user sends you." },
+    { role: "user", content: "" },
   ];
-  
 
   constructor() { }
 
@@ -24,19 +31,40 @@ export class LlmService {
     this.generator = await pipeline(
       "text-generation",
       "onnx-community/Qwen2.5-0.5B-Instruct",
-      { dtype: "q4", device: "webgpu" },
-    );
+      { 
+        dtype: "q4", 
+        device: "webgpu",
+        progress_callback: () => void 0
+      },
+    )
   }
 
-  async generate() : Promise<void>{
+  parseGeneratedText(generatedData : TextGenerationOutput | TextGenerationOutput[]) : string{
+
+    if (!("generated_text" in generatedData[0]) 
+        || generatedData[0].generated_text.length < 3
+        || !("content" in (generatedData[0].generated_text as Chat)[2])
+    ) throw new Error("Invalid generated data format.")
+
+    return (generatedData[0].generated_text as Chat)[2].content
+  }
+
+  async generate(description : string) : Promise<void>{
     try{
       await this.init()
+
+      this.output$.next("")
       if(!this.generator) throw new Error("LLM generator hasn't been initilized.")
-      const output = await this.generator(this.messages, { max_new_tokens: 128 });
-      this.output$.next(JSON.stringify(output[0]))
-      console.log(JSON.stringify(output[0])); // .generated_text.at(-1).content
+
+      const sumConv = [...this.messages]
+      sumConv[1] = {...sumConv[1], content : `Text to summarize : ${description}`}
+      const generatedData = await this.generator(sumConv, { max_new_tokens: 128 })
+
+      this.output$.next(this.parseGeneratedText(generatedData))
+      // console.log(JSON.stringify(generatedData[0])) // .generated_text.at(-1).content
     }catch(error : unknown){
       console.error(error)
+      this.output$.next("")
     }
   }
 }
