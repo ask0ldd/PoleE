b8b86f3d20848bfaca9f2a0495c3543cacf6e876
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-04-25 02:30:42 +0200

    *edit* front / llm service :
    - protecting the output subject using an obs
    - fixing the busy state tracking update

--- front/src/app/services/llm.service.ts
+++ front/src/app/services/llm.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
-import { pipeline, TextGenerationPipeline, env, Chat, TextGenerationOutput } from '@huggingface/transformers'
-import { map, Subject } from 'rxjs';
+import { pipeline, TextGenerationPipeline, env, Chat, TextGenerationOutput, ProgressCallback } from '@huggingface/transformers'
+import { Subject } from 'rxjs';
 
 // https://github.com/huggingface/transformers.js/blob/main/docs/source/pipelines.md
 
@@ -14,32 +14,72 @@ env.localModelPath = 'http://localhost:3000/static/model'
 })
 export class LlmService {
 
-  output$ = new Subject<string>()
-  i : number = 0
+  outputSubject = new Subject<string>()
+  output$ = this.outputSubject.asObservable()
+  isBusy = false
 
-  generator : TextGenerationPipeline | null = null
-  messages = [
-    { role: "system", content: "You are a helpful assistant. Your goal is to summarize any text the user sends you." },
-    { role: "user", content: "" },
-  ];
+  private generator : TextGenerationPipeline | null = null
+  private conversation = new Conversation("You are a helpful assistant. Your goal is to summarize as a list of bullet points any text the user sends you.")
 
-  constructor() { }
+  private progress_callback : ProgressCallback = (progress) => {
+    if(!progress.status) return
+    if(progress.status == "initiate") {
+      this.isBusy = true
+      console.log("initiate")
+    }
+    if(progress.status == "done") {
+      this.isBusy = false
+      console.log("done")
+    }
+    if(progress.status == "ready") {
+      this.isBusy = false
+      console.log("ready")
+    }
+  }
 
-  async init(){
+  private async init() : Promise<void>{
     if(this.generator != null) return
     // @ts-ignore
     this.generator = await pipeline(
       "text-generation",
       "onnx-community/Qwen2.5-0.5B-Instruct",
+      // "onnx-community/gemma-3-1b-it-ONNX",
       { 
         dtype: "q4", 
         device: "webgpu",
-        progress_callback: () => void 0
+        progress_callback: this.progress_callback
       },
     )
   }
 
-  parseGeneratedText(generatedData : TextGenerationOutput | TextGenerationOutput[]) : string{
+  async generate(description : string) : Promise<void> {
+    try{
+      console.log("generate + busy : ", this.isBusy)
+
+      if(this.isBusy) return
+
+      await this.init()
+
+      this.outputSubject.next("")
+      if(!this.generator) throw new Error("LLM generator hasn't been initilized.")
+
+      this.conversation.setUserMessage(`Text to summarize : ${description}`)
+
+      this.isBusy = true // !! should interrupt / give choice?
+      const generatedData = await this.generator(this.conversation.get(), { max_new_tokens: 128, /*do_sample: false */})
+
+      this.isBusy = false
+
+      this.outputSubject.next(this.parseGeneratedText(generatedData))
+      // console.log(generatedData)
+      // console.log(JSON.stringify(generatedData[0])) // .generated_text.at(-1).content
+    }catch(error : unknown){
+      console.error(error)
+      this.outputSubject.next("")
+    }
+  }
+
+  private parseGeneratedText(generatedData : TextGenerationOutput | TextGenerationOutput[]) : string {
 
     if (!("generated_text" in generatedData[0]) 
         || generatedData[0].generated_text.length < 3
@@ -49,22 +89,29 @@ export class LlmService {
     return (generatedData[0].generated_text as Chat)[2].content
   }
 
-  async generate(description : string) : Promise<void>{
-    try{
-      await this.init()
+}
 
-      this.output$.next("")
-      if(!this.generator) throw new Error("LLM generator hasn't been initilized.")
+class Conversation{
 
-      const sumConv = [...this.messages]
-      sumConv[1] = {...sumConv[1], content : `Text to summarize : ${description}`}
-      const generatedData = await this.generator(sumConv, { max_new_tokens: 128 })
+  constructor(systemPrompt? : string){
+    this.setSystemPrompt(systemPrompt ?? "You are a helpful assistant.")
+  }
 
-      this.output$.next(this.parseGeneratedText(generatedData))
-      // console.log(JSON.stringify(generatedData[0])) // .generated_text.at(-1).content
-    }catch(error : unknown){
-      console.error(error)
-      this.output$.next("")
-    }
+  private messages : Chat = [
+    { role: "system", content: "You are a helpful assistant. Your goal is to summarize any text the user sends you." },
+    { role: "user", content: "" },
+  ];
+
+  setUserMessage(message : string) : void {
+    const nMessages = this.messages.length
+    this.messages[nMessages - 1].content = message
+  }
+
+  setSystemPrompt(prompt : string) : void {
+    this.messages[0].content = prompt
+  }
+
+  get() : Chat {
+    return this.messages
   }
 }
