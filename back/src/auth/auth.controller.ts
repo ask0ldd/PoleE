725f0680830ae8882da489af423e3f518698edd0
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-04-28 20:41:21 +0200

    *add* back / adding auth controller / service / module

---
+++ back/src/auth/auth.controller.ts
@@ -0,0 +1,47 @@
+import { BadRequestException, Body, Controller, Get, HttpCode, HttpStatus, Post, UseGuards, Request, NotFoundException } from '@nestjs/common';
+import { AuthService } from './auth.service';
+import { User } from 'src/users/entities/user.entity';
+import { UsersService } from 'src/users/users.service';
+import { AuthGuard } from './auth.guard';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService, private usersService: UsersService) {}
+
+    @HttpCode(HttpStatus.OK)
+    @Post('login')
+    signIn(@Body() signInDto: {username : string, password : string}) {
+
+        const { username, password } = signInDto
+        
+        if (!username || !password) {
+            // will respond with a 400 Bad Request
+            throw new BadRequestException('Username and password are required')
+        }
+        
+        return this.authService.signIn(username, password)
+    }
+
+    @UseGuards(AuthGuard)
+    @Get('profile')
+    async getProfile(@Request() req : {user : string}) : Promise<User> { // @Request() req : {user: string} set by middleware / guard
+          const username = req.user
+
+          if (!username) {
+            throw new BadRequestException('Missing required request parameter: user');
+          }
+
+          const user = await this.usersService.findByUsername(username)
+          if(!user) {
+            throw new NotFoundException(`User named ${username} not found`)
+          }
+
+          return user
+    }
+
+    @UseGuards(AuthGuard)
+    @Get('test')
+    getTest(@Body() testDto : {user: string}) : string{
+        return testDto.user
+    }
+}
---
+++ back/src/auth/auth.service.ts
@@ -0,0 +1,38 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { UsersService } from 'src/users/users.service';
+import * as bcrypt from 'bcrypt';
+
+@Injectable()
+export class AuthService {
+    constructor(
+        private usersService: UsersService,
+        private jwtService: JwtService
+    ) {}
+    
+    async signIn(username: string, pass: string): Promise<{ access_token: string }> {
+
+        const user = await this.usersService.findByUsername(username)
+
+        if(!user?.password) throw new Error("User has no password")
+
+        if (!this.comparePasswords(pass, user.password)) {
+            throw new UnauthorizedException()
+        }
+
+        const payload = { 
+            sub: user.id, 
+            username: user.username, 
+            admin : false, 
+            iss: 'your-issuer-name' 
+        }
+
+        return {
+            access_token: await this.jwtService.signAsync(payload),
+        }
+    }
+
+    comparePasswords(plainPassword: string, hashedPassword: string): boolean {
+        return bcrypt.compareSync(plainPassword, hashedPassword)
+    }
+}
