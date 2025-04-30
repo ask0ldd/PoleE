a96e9730119bcf2abd4b38a70a37bce5c7700e7b
Cedric B-Eff <cedrikopenclass@gmx.com>
2025-04-30 22:11:21 +0200

    *edit* front / fixing login page layout

--- front/src/app/components/header/header.component.ts
+++ front/src/app/components/header/header.component.ts
@@ -1,10 +1,10 @@
 import { Component } from '@angular/core';
 import { AuthService } from '../../services/auth.service';
-import { Router } from '@angular/router';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
@@ -12,7 +12,4 @@ export class HeaderComponent {
 
   constructor(private authService : AuthService, private router: Router) { }
 
-  handleLogin(){
-    this.router.navigate(['/login'])
-  }
 }
